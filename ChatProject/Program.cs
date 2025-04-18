8ea9ba7bc6d05ef4b6a4ff0dee82515e5bb8d4fc
Vilens Senat <n01064501@unf.edu>
2025-04-17 23:45:35 -0400

    added my local database

--- ChatProject/Program.cs
+++ ChatProject/Program.cs
@@ -22,7 +22,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(UserMappingProfile));
 
 //dotnet add package Microsoft.EntityFrameworkCore.InMemory
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+//sql server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IAppDbContext, AppDbContext>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IChatService, ChatService>();
